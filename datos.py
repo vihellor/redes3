2ab70fc977ece06acc6a3f6a38b55fc9637a4a3f
vihellor <vihellor@gmail.com>
2017-09-26 20:03:50 -0500

    Create datos.py

---
+++ datos.py
@@ -0,0 +1,26 @@
+from pysnmp.hlapi import *
+def datos(comunidad, ip):
+  errorIndication, errorStatus, errorIndex, varBinds = next(
+      nextCmd(SnmpEngine(),
+             CommunityData(comunidad, mpModel=0),
+             UdpTransportTarget((ip, 161)),
+             ContextData(),
+             ObjectType(ObjectIdentity('1.3.6.1.2.1.1.1.0')),
+             ObjectType(ObjectIdentity('1.3.6.1.2.1.1.2.0')),
+             ObjectType(ObjectIdentity('1.3.6.1.2.1.1.3.0')),
+             ObjectType(ObjectIdentity('1.3.6.1.2.1.1.4.0')),
+             ObjectType(ObjectIdentity('1.3.6.1.2.1.1.5.0')),
+             ObjectType(ObjectIdentity('1.3.6.1.2.1.1.6.0')))
+  )
+
+  if errorIndication:
+      print(errorIndication)
+  elif errorStatus:
+      print('%s at %s' % (errorStatus.prettyPrint(),
+                          errorIndex and varBinds[int(errorIndex) - 1][0] or '?'))
+  else:
+      for varBind in varBinds:
+          print(' = '.join([x.prettyPrint() for x in varBind]))
+      return varBinds
+
+datos("comunSNMP","localhost")
