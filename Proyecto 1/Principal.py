3a0859532d7739836801bddc2b0999e79a7b5209
vihellor <vihellor@gmail.com>
2017-10-01 20:30:39 -0500

    correccion proyecto.py y agregar datos() como metodo en principal

--- Proyecto 1/Principal.py
+++ Proyecto 1/Principal.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 from datos import datos 
 from Agregar import Agregar
+from pysnmp.hlapi import *
 
 class Proyecto(tk.Frame):
 	def __init__(self, parent):
@@ -38,6 +39,35 @@ class Proyecto(tk.Frame):
 
 	def ver(self):
 		print('Ip: ')
+        vis = new Visual()
+        #Visual.an()  ##sólo falta agregar el 
+    def datos(comunidad, ip):
+	    errorIndication, errorStatus, errorIndex, varBinds = next(
+		    nextCmd(SnmpEngine(),
+		    CommunityData(comunidad, mpModel=0),
+		    UdpTransportTarget((ip, 161)),
+		    ContextData(),
+		    ObjectType(ObjectIdentity('1.3.6.1.2.1.1.1.0')),
+		    ObjectType(ObjectIdentity('1.3.6.1.2.1.1.2.0')),
+		    ObjectType(ObjectIdentity('1.3.6.1.2.1.1.3.0')),
+		    ObjectType(ObjectIdentity('1.3.6.1.2.1.1.4.0')),
+		    ObjectType(ObjectIdentity('1.3.6.1.2.1.1.5.0')),
+		    ObjectType(ObjectIdentity('1.3.6.1.2.1.1.6.0'))))
+
+	    if errorIndication:
+		    print(errorIndication)
+		    return None
+
+	    elif errorStatus:
+		    print('%s at %s' % (errorStatus.prettyPrint(), errorIndex and varBinds[int(errorIndex) - 1][0] or '?'))
+		    return None
+		
+	    else:
+		    datos = '' 
+		    for varBind in varBinds:
+			    data = str(varBind).split('=')
+			    datos = datos + ' ' + data[1] 
+		    return datos
 
 
 def main():
